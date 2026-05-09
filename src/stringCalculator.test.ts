7991af2ab2f00e13e07dc02de696e1050d8ffc01
krunal-vyas <vyaskrunal300@gmail.com>
2026-05-09 12:27:10 +0530

    test: single number returns itself

--- src/stringCalculator.test.ts
+++ src/stringCalculator.test.ts
@@ -6,4 +6,9 @@ describe('add()', () => {
     expect(add('')).toBe(0);
   });
 
+  it('returns the number itself for a single value', () => {
+    expect(add('1')).toBe(1);
+    expect(add('9')).toBe(9);
+  });
+
 });
