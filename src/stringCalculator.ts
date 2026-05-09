861c9cc3c3c0d34f2a4f493e507ae908bb71af5f
krunal-vyas <vyaskrunal300@gmail.com>
2026-05-09 12:40:30 +0530

    feat: parse custom delimiter from // header

--- src/stringCalculator.ts
+++ src/stringCalculator.ts
@@ -3,6 +3,20 @@
 
 export function add(input: string): number {
   if (input === '') return 0;
-  return input.split(/,|\n/).map(Number).reduce((a, b) => a + b, 0);
+
+  let delimiter: RegExp = /,|\n/;
+  let numbers = input;
+
+  if (input.startsWith('//')) {
+    const newlineIndex = input.indexOf('\n');
+    const header = input.substring(2, newlineIndex);
+    numbers = input.substring(newlineIndex + 1);
+    delimiter = new RegExp(escape(header));
+  }
+
+  return numbers.split(delimiter).map(Number).reduce((a, b) => a + b, 0);
 }
 
+function escape(str: string): string {
+  return str.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
+}
