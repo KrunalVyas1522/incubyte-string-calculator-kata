0593240feaf8b0d7dec03c71cfc35026182a6e34
krunal-vyas <vyaskrunal300@gmail.com>
2026-05-09 12:38:44 +0530

    test: newline should be treated as delimiter

--- src/stringCalculator.test.ts
+++ src/stringCalculator.test.ts
@@ -19,5 +19,9 @@ describe('add()', () => {
     expect(add('1,2,3,4,5')).toBe(15);
   });
 
+  it('treats newlines as valid delimiters', () => {
+    expect(add('1\n2,3')).toBe(6);
+    expect(add('1\n2\n3')).toBe(6);
+  });
 
 });
